9ccadc68f5141262fa47abf260885c2ed2b26264
cddwx <cddwx525@163.com>
2017-11-12 17:49:28 +0800

    More robost judgement for running start message and running finished message.

--- serial_read_thread.py
+++ serial_read_thread.py
@@ -20,29 +20,49 @@ class serial_read_thread(threading.Thread):
 
     def run(self):
         while (not self.event_stop.is_set()):
-            try:
-                '''
-                This line always cause a IOErroro when I close serial port, so I catch it and exit thread.
-                Traceback (most recent call last):
-                    ...
-                  File "/usr/local/lib/python2.7/dist-packages/serial/serialposix.py", line 464, in in_waiting
-                      s = fcntl.ioctl(self.fd, TIOCINQ, TIOCM_zero_str)
-                  IOError: [Errno 9] Bad file descriptor
-                '''
-                in_waiting = self.serial_port.inWaiting()
-            except (ValueError, Exception) as e:
-                return
-
-
             if (self.event_run_seq.is_set() == True):
-                #print in_waiting
-                try:
-                    text = self.serial_port.read(len("Running ...\x0D\x0A"))
-                except (ValueError, Exception) as e:
-                    wx.CallAfter(pub.sendMessage, "serial_read_error", data = e.message)
+                count = 0
+                text = ""
+                while (
+                        (count < len("Running ...\x0D\x0A")) and
+                        (self.event_run_seq.is_set() == True)
+                ):
+                    try:
+                        in_waiting = self.serial_port.inWaiting()
+                    except (IOError) as e:
+                        print "Serial_read_thread.run in_waiting: " + e.message
+                        wx.CallAfter(pub.sendMessage, "serial_read_error", data = e.message)
+                        continue
+
+                    if (in_waiting > 0):
+                        if ((count + in_waiting) <= len("Running ...\x0D\x0A")):
+                            #print in_waiting
+                            try:
+                                one_text = self.serial_port.read(in_waiting)
+                            except (ValueError, Exception) as e:
+                                wx.CallAfter(pub.sendMessage, "serial_read_error", data = e.message)
+
+                            text = text + one_text
+                            count = count + in_waiting
+                        else:
+                            try:
+                                one_text = self.serial_port.read(len("Running ...\x0D\x0A") - count)
+                            except (ValueError, Exception) as e:
+                                wx.CallAfter(pub.sendMessage, "serial_read_error", data = e.message)
+
+                            text = text + one_text
+                            break
+                    else:
+                        pass
+
+                    time.sleep(0.01)
 
                 wx.CallAfter(pub.sendMessage, "update", data = text)
 
+                if (text == "Running ...\x0D\x0A"):
+                    pass
+                else:
+                    continue
 
                 count = 0
                 text = ""
@@ -52,31 +72,51 @@ class serial_read_thread(threading.Thread):
                 ):
                     try:
                         in_waiting = self.serial_port.inWaiting()
-                    except (ValueError, Exception) as e:
-                        return
+                    except (IOError) as e:
+                        print "Serial_read_thread.run in_waiting: " + e.message
+                        wx.CallAfter(pub.sendMessage, "serial_read_error", data = e.message)
+                        continue
 
                     if (in_waiting > 0):
-                        #print in_waiting
-                        try:
-                            text = text + self.serial_port.read(in_waiting)
-                        except (ValueError, Exception) as e:
-                            wx.CallAfter(pub.sendMessage, "serial_read_error", data = e.message)
+                        if ((count + in_waiting) <= len("Running finished.\x0D\x0A\x0D\x0A")):
+                            #print in_waiting
+                            try:
+                                one_text = self.serial_port.read(in_waiting)
+                            except (ValueError, Exception) as e:
+                                wx.CallAfter(pub.sendMessage, "serial_read_error", data = e.message)
 
-                        count = count + in_waiting
+                            text = text + one_text
+                            count = count + in_waiting
+                        else:
+                            try:
+                                one_text = self.serial_port.read(len("Running finished.\x0D\x0A\x0D\x0A") - count)
+                            except (ValueError, Exception) as e:
+                                wx.CallAfter(pub.sendMessage, "serial_read_error", data = e.message)
+
+                            text = text + one_text
+                            break
                     else:
                         pass
 
                     time.sleep(0.01)
 
+                wx.CallAfter(pub.sendMessage, "update", data = text)
+
                 if (text == "Running finished.\x0D\x0A\x0D\x0A"):
-                    wx.CallAfter(pub.sendMessage, "update", data = text)
                     self.event_run_finished.set()
                 else:
-                    pass
+                    continue
             else:
+                try:
+                    in_waiting = self.serial_port.inWaiting()
+                except (IOError) as e:
+                    print "Serial_read_thread.run in_waiting: " + e.message
+                    continue
+
                 if (in_waiting > 0):
                     try:
                         text = self.serial_port.read(in_waiting)
+                        #print type(text)
                     except (ValueError, Exception) as e:
                         wx.CallAfter(pub.sendMessage, "serial_read_error", data = e.message)
                     else:
